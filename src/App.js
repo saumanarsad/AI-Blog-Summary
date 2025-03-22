1e20f9618f7149f1660d75e80a9474a6e5adabec
saumanarsad <sauman.arshad@gmail.com>
2025-03-22 17:40:20 +0500

    Configure tailwind

--- src/App.js
+++ src/App.js
@@ -3,22 +3,11 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='flex flex-col items-center justify-center min-h-screen bg-gray-900 text-white'>
+      <h1 className='text-4xl font-bold'>AI Powered Blog Summary</h1>
+      <p className="text-lg mt-2">Paste a blog link, and let AI generate a summary!</p>
     </div>
+
   );
 }
 
